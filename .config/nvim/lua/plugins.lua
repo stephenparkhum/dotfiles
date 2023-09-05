686270b80aabd0584d4a5a7e4dc72b3e8564cac4
Stephen Parker <stephenparkhum@outlook.com>
2023-09-05 08:52:43 -0700

    Updating neotree [+] sign

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -53,6 +53,10 @@ return {
         close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
         popup_border_style = "rounded",
         enable_git_status = true,
+        modified = {
+          symbol = "[+]",
+          highlight = "NeoTreeModified",
+        },
         git_status = {
           window = {
             position = "float",
