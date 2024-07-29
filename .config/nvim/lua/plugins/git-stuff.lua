43cb861ac2c15e751f99d038f3031f6e5352c3ef
Stephen Parker <Stephen.Parker@moodys.com>
2024-07-29 08:37:15 -0700

    updating plugins, adding git stuff, and updating alacritty

--- .config/nvim/lua/plugins/git-stuff.lua
+++ .config/nvim/lua/plugins/git-stuff.lua
@@ -1,14 +1,41 @@
 return {
-    {
-        "tpope/vim-fugitive"
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup()
+	{
+		"f-person/git-blame.nvim",
+		config = function()
+			require("gitblame").setup({
+				delay = 1000,
+				enabled = true,
+				message_template = "<committer> • <date> • <sha>",
+				message_when_not_committed = "nah",
+			})
 
-            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-        end
-    }
+			vim.keymap.set("n", "<leader>gb", "<CMD>GitBlameToggle<CR>", { desc = "Toggle git blame" })
+			vim.keymap.set("n", "<leader>gof", "<CMD>GitBlameOpenFileURL<CR>", { desc = "Open file in GitHub" })
+			vim.keymap.set(
+				"n",
+				"<leader>goc",
+				"<CMD>GitBlameOpenCommitURL<CR>",
+				{ desc = "Open commit of current line in GitHub" }
+			)
+			vim.keymap.set("n", "<leader>gcf", "<CMD>GitBlameCopyFileURL<CR>", { desc = "Copy github url of file" })
+			vim.keymap.set(
+				"n",
+				"<leader>gcc",
+				"<CMD>GitBlameCopyCommitURL<CR>",
+				{ desc = "Copy github url of current line's commit" }
+			)
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
+	},
 }
