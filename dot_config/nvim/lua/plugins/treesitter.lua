0617278b38c24707fd9781aed6bb7f9247268bc9
sp <stephenparkhum@outlook.com>
2024-10-16 22:49:47 -0700

    Removing stuff I didn't use

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -7,55 +7,92 @@ return {
 		"nvim-treesitter/nvim-treesitter-context",
 	},
 	config = function(_)
-		require("nvim-treesitter.configs").setup(
-			{
-				event = { "BufReadPre", "BufNewFile" },
-				ensure_installed = {
-					"css",
-					"devicetree",
-					"html",
-					"go",
-					"javascript",
-					"jsdoc",
-					"lua",
-					"markdown",
-					"markdown_inline",
-					"python",
-					"scss",
-					"rust",
-					"typescript",
-					"tsx",
-					"vim",
-					"vimdoc",
-				},
-				sync_install = true,
-				query_linter = {
+		require("nvim-treesitter.configs").setup({
+			event = { "BufReadPre", "BufNewFile" },
+			ensure_installed = {
+				"css",
+				"devicetree",
+				"html",
+				"javascript",
+				"jsdoc",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"scss",
+				"rust",
+				"typescript",
+				"tsx",
+				"vim",
+				"vimdoc",
+			},
+			sync_install = true,
+			query_linter = {
+				enable = true,
+				use_virtual_text = true,
+				lint_events = { "BufWrite", "CursorHold" },
+			},
+			textobjects = {
+				select = {
 					enable = true,
-					use_virtual_text = true,
-					lint_events = { "BufWrite", "CursorHold" },
-				},
-				incremental_selection = {
-					enable = true,
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
 					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						-- You can optionally set descriptions to the mappings (used in the desc parameter of
+						-- nvim_buf_set_keymap) which plugins like which-key display
+						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+						-- You can also use captures from other query groups like `locals.scm`
+						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
 					},
+					-- You can choose the select mode (default is charwise 'v')
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * method: eg 'v' or 'o'
+					-- and should return the mode ('v', 'V', or '<c-v>') or a table
+					-- mapping query_strings to modes.
+					selection_modes = {
+						["@parameter.outer"] = "v", -- charwise
+						["@function.outer"] = "V", -- linewise
+						["@class.outer"] = "<c-v>", -- blockwise
+					},
+					-- If you set this to `true` (default is `false`) then any textobject is
+					-- extended to include preceding or succeeding whitespace. Succeeding
+					-- whitespace has priority in order to act similarly to eg the built-in
+					-- `ap`.
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * selection_mode: eg 'v'
+					-- and should return true or false
+					include_surrounding_whitespace = true,
 				},
-				modules = {},
-				auto_install = true,
-				indent = {
-					enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
 				},
-				highlight = {
-					enable = true,
-					use_languagetree = true,
-				},
-			}
-		)
+			},
+			modules = {},
+			auto_install = true,
+			indent = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+				use_languagetree = true,
+			},
+		})
 		require("treesitter-context").setup({
-			enable = false,
+			enable = true,
 			max_lines = 0,
 			min_window_height = 0,
 			line_numbers = true,
