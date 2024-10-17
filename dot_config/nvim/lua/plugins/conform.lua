dbe1326bb2f9952102b45b5d684dc42a072664a0
sp <stephenparkhum@outlook.com>
2024-10-16 22:13:21 -0700

    updating formatting, removing plugins, etc.

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,28 +1,29 @@
 return {
-  'stevearc/conform.nvim',
-  lazy = false,
-  keys = {
-    {
-      "<leader>f",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "[F]ormat Buffer"
-    },
-  },
-  opts = {
-    notify_on_error = false,
-    format_on_save = {
-      lsp_fallback = true,
-      async = false,
-      timeout_ms = 500,
-    },
-    formatters_by_ft = {
-      lua = { "stylua" },
-      css = { { "prettierd", "prettier" } },
-      javascript = { { "prettierd", "prettier" } },
-      rust = { { "prettierd", "prettier" } },
-    },
-  },
+	"stevearc/conform.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "[F]ormat Buffer",
+		},
+	},
+	opts = {
+		notify_on_error = false,
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 500,
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			css = { { "prettierd", "prettier" } },
+			javascript = { { "prettierd", "prettier" } },
+			rust = { "rustfmt", lsp_format = "fallback" },
+		},
+	},
 }
