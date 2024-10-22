b56d7d9b57e40492fba7e8daabc3ad41c9413d1f
sp <stephenparkhum@outlook.com>
2024-10-21 19:45:45 -0700

    Updating plugins and telescope items

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -71,6 +71,10 @@ return {
 			},
 		})
 
+		local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+		vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+		vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
 		require("telescope").load_extension("fzf")
 		require("telescope").load_extension("file_browser")
 		require("telescope").load_extension("scope")
