f7dfd803673b50a756d7f064864bf8e6218bfd45
sp <stephenparkhum@outlook.com>
2024-10-29 22:07:49 -0700

    new theme for alacritty

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,9 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  opts = {
-    options = {
-      theme = 'catppuccin',
-    },
-    sections = {
-      lualine_x = { 'format', 'filetype' },
-      lualine_y = { 'progress' },
-    },
-  }
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		sections = {
+			lualine_x = { "format", "filetype" },
+			lualine_y = { "progress" },
+		},
+	},
 }
