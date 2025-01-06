fca9da79ddb893960815b34834acc03e38c954e9
sp <stephenparkhum@outlook.com>
2025-01-05 21:26:37 -0800

    Updating mini

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,6 @@ return {
 	"echasnovski/mini.nvim",
 	config = function()
 		-- Better Around/Inside textobjects
-		--
 		-- Examples:
 		--  - va)  - [V]isually select [A]round [)]paren
 		--  - yinq - [Y]ank [I]nside [N]ext [']quote
@@ -10,11 +9,10 @@ return {
 		require("mini.ai").setup({ n_lines = 500 })
 
 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-		--
 		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
-		require("mini.surround").setup()
+		require("mini.surround").setup({})
 		local MiniStatusline = require("mini.statusline")
 		MiniStatusline.setup({
 			content = {
